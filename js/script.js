8be57e1778379b0f4d9591b7da83bb087b5fda9e
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-07 16:45:59 +0100

    feat: add 'unpacked' problem

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Stampare numeri da 1 a 100
+  //Creo ciclo
+  for (i = 1; i <= 1000; i++) {
+    let multipleOf3 = 'Fizz';
+    let multipleOf5 = 'Buzz';
+  };
+  //Multipli di 3 scitta Fizz invece di numero
+    //Creo variabile con stringa 'Fizz' da inserire al posto di numero
+    //Numero è multiplo di 3 se dividendolo per 3 da resto 0 ( numero % 3)
+  //Multipli di 5 scitta Buzz invece di numero
+    //Creo variabile con stringa 'BUzz' da inserire al posto di numero
+    //Numero è multiplo di 5 se dividendolo per 5 da resto 0 ( numero % 5)
+  //Multipli sia di 3 che di 5 scritta FizzBuzz invece di numero
+    //Creo variabile con stringa 'FizzBuzz' da inserire al posto di numero
+    //Numero è multiplo di 3 e di 5 se dividendolo per 3 e/o per 5 da resto 0 ( numero % 3/numero % 5)
+//Pongo le condizioni logiche
