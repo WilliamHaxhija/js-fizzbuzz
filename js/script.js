b6eede0fe4b97b0c54a7186869bc12826f1da83e
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-07 19:16:17 +0100

    feat: add bonus 2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector('.container');
 for (let i = 1; i <= 100; i++) {
     let fizzOrBuzz;
+    let backGround;
     if (i % 3 === 0 && i % 5 === 0) {
         fizzOrBuzz = 'FizzBuzz';
+        backGround = 'fizz-buzz';
     } else if (i % 3 === 0) {
         fizzOrBuzz = 'Fizz';
+        backGround = 'fizz';
     } else if (i % 5 === 0) {
         fizzOrBuzz = 'Buzz';
+        backGround = 'buzz';
     } else {
         fizzOrBuzz = i;
     }
     const box = document.createElement('div');
     box.innerHTML = fizzOrBuzz;
+    box.classList.add('card');
+    box.classList.add(backGround);
     container.append(box);
 }
 
