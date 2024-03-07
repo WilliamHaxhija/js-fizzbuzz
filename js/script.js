310fe329fa73a2920e28ebf69bb74d3b4556c411
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-07 18:34:42 +0100

    feat: add bonus 1

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
-//Stampare numeri da 1 a 100
-  //Creo ciclo
-   //Multipli di 3 scitta Fizz invece di numero
-    //Creo variabile con stringa 'Fizz' da inserire al posto di numero
-    //Numero è multiplo di 3 se dividendolo per 3 da resto 0 ( numero % 3)
-  //Multipli di 5 scitta Buzz invece di numero
-    //Creo variabile con stringa 'BUzz' da inserire al posto di numero
-    //Numero è multiplo di 5 se dividendolo per 5 da resto 0 ( numero % 5)
-  //Multipli sia di 3 che di 5 scritta FizzBuzz invece di numero
-    //Creo variabile con stringa 'FizzBuzz' da inserire al posto di numero
-    //Numero è multiplo di 3 e di 5 se dividendolo per 3 e/o per 5 da resto 0 ( numero % 3/numero % 5)
-//Pongo le condizioni logiche
-  for (let i = 1; i <= 100; i++) {
+const container = document.querySelector('.container');
+for (let i = 1; i <= 100; i++) {
+    let fizzOrBuzz;
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
+        fizzOrBuzz = 'FizzBuzz';
     } else if (i % 3 === 0) {
-        console.log('Fizz');
+        fizzOrBuzz = 'Fizz';
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+        fizzOrBuzz = 'Buzz';
     } else {
-        console.log(i);
+        fizzOrBuzz = i;
     }
-  }
+    const box = document.createElement('div');
+    box.innerHTML = fizzOrBuzz;
+    container.append(box);
+}
+
  
